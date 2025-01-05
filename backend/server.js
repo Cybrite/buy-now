58ed320648176be12880262122c6511759e75a90
Harsh <harshtanishq2002@gmail.com>
2025-01-06 00:41:48 +0530

    feat: added product schema

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,35 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send("Hello from the backend")
+app.get("/", (req, res) => {
+  res.send("Hello from the backend");
 });
 
 app.listen(5000, () => {
-    connectDB();
-    console.log("Server is running on http://localhost:5000 🚀");
+  connectDB();
+  console.log("Server is running on http://localhost:5000 🚀");
 });
 
+app.post("/products", async (req, res) => {
+  const product = await req.body;
+
+  if (product.name === "" || product.price === "" || product.image === "") {
+    return res.status(400).json({ message: "Please fill all the fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, product: newProduct });
+  } catch (error) {
+    console.log(`Error in creating a product: ${error.message}`);
+    res.status(500).json({ success: false, message: "server error" });
+  }
+});
