4629fd5f2c91b232fb8c6ca63fe29264cd03a685
Harsh <harshtanishq2002@gmail.com>
2025-01-08 00:22:11 +0530

    feat: added deletion of products

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,16 @@ app.listen(5000, () => {
   console.log("Server is running on http://localhost:5000 🚀");
 });
 
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, products });
+  } catch (error) {
+    console.log(`Error in fetching products: ${error.message}`);
+    res.status(500).json({ success: false, message: "server error" });
+  }
+});
+
 app.post("/api/products", async (req, res) => {
   const product = await req.body;
 
@@ -31,6 +41,18 @@ app.post("/api/products", async (req, res) => {
     res.status(201).json({ success: true, product: newProduct });
   } catch (error) {
     console.log(`Error in creating a product: ${error.message}`);
+    res.status(500).json({ success: false, message: "server error" });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted" });
+  } catch (error) {
+    console.log(`Error in deleting a product: ${error.message}`);
     res.status(500).json({ success: false, message: "server error" });
   }
 });
